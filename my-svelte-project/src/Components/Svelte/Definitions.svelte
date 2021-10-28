5c77222afa85aeecac7ca9f96f25ab58e6a40819
Bettina Carrizo <betticarrizo@gmail.com>
2021-10-28 20:30:27 -0300

    Svelte - Intro y Quizz

--- my-svelte-project/src/Components/Svelte/Definitions.svelte
+++ my-svelte-project/src/Components/Svelte/Definitions.svelte
@@ -1,4 +1,40 @@
 <script>
+    import Card from "./Card.svelte";
+
+    let framework;
+    let libreria;
+
+    const definitions = {
+        framework: {
+            title: 'Framework',
+            definition: 'Los frameworks son un conjunto de funciones que nos permiten a los devs abstraernos de estar pendientes de cuándo son invocadas ciertas acciones y cómo es que se configuran. Éstos brindan una API amigable que nos permite configurar el comportamiento de nuestra app, pero es el framework el que tiene el control sobre ello.'
+        },
+        libreria: {
+            title: 'Libreria',
+            definition: 'Las librerías son conjuntos de funciones que nosotros debemos invocar cuando querramos hacer uso de ellas. Un ejemplo claro es la libreria java.util.Date que nos permite acceder a funciones de fecha, pero solo son ejecutadas cuando nosotros directamente lo codificamos.'
+        }
+    }
+
+    $: {
+        framework = definitions.framework.title
+        libreria = definitions.libreria.title
+    }
+
+    function onFrameworkClick() {
+        if (framework == definitions.framework.title) {
+            framework = definitions.framework.definition
+        } else {
+            framework = definitions.framework.title
+        }
+    }
+
+    function onLibraryClick() {
+        if (libreria == definitions.libreria.title) {
+            libreria = definitions.libreria.definition
+        } else {
+            libreria = definitions.libreria.title
+        }
+    }
 
 </script>
 
@@ -8,12 +44,12 @@
 <article class="p-5 mb-5 bg-red-200">
     <h2 class="text-2xl mb-3">Framework o Libreria?</h2>
     
-    <div>
-        <div>
-        <p>Framework</p>
-        </div>
-        <div>
-        <p>Libreria</p>
-        </div>
+    <div class="flex space-x-6">
+        <Card 
+            title={framework}
+            onClick={onFrameworkClick} />
+        <Card 
+            onClick={onLibraryClick}
+            title={libreria}/>
     </div>
 </article>
--- my-svelte-project/src/Components/Svelte/Quizz.svelte
+++ my-svelte-project/src/Components/Svelte/Quizz.svelte
@@ -1,21 +1,37 @@
 <script>
     import ImageCard from "./ImageCard.svelte";
 
+    const colors = {
+        default: 'purple',
+        error: 'red',
+        success: 'green'
+    }
+
     const quizzCards = {
         framework: {
             title: 'Framework',
             imageUrl: 'https://media.giphy.com/media/7J1llSYGJhKQJpVS9V/giphy.gif',
-            imageAlt: 'Gif de Alf filmando con una cámara'
+            imageAlt: 'Gif de Alf filmando con una cámara',
+            color: colors.default
         },
         libreria: {
             title: 'Libreria',
             imageUrl: 'https://media.giphy.com/media/Jn6UleOajZxAY/giphy.gif',
-            imageAlt: 'Gif de Alf mirando una película'
+            imageAlt: 'Gif de Alf mirando una película',
+            color: colors.default
         }
     }
 
-    function onAnswerSelection() {
+    function onFrameworkClick() {
+        quizzCards.framework.imageUrl = "https://media.giphy.com/media/KY41NM9XNzWy4/giphy.gif"
+        quizzCards.framework.imageAlt = "Gif de Alf cantando feliz"
+        quizzCards.framework.color = colors.success
+    }
 
+    function onLibraryClick() {
+        quizzCards.libreria.imageUrl = "https://media.giphy.com/media/NDvZ2XKsfFm24/giphy.gif"
+        quizzCards.libreria.imageAlt = "Gif de Alf cuando explota la cocina y sale volando"
+        quizzCards.libreria.color = colors.default
     }
 </script>
 
@@ -28,12 +44,16 @@
         <ImageCard 
         title={quizzCards.framework.title}
         imageUrl={quizzCards.framework.imageUrl}
-        imageAlt={quizzCards.framework.imageAlt} />
+        imageAlt={quizzCards.framework.imageAlt}
+        color={quizzCards.framework.color}
+        onClick={onFrameworkClick} />
 
         <ImageCard 
         title={quizzCards.libreria.title}
         imageUrl={quizzCards.libreria.imageUrl}
         imageAlt={quizzCards.libreria.imageAlt}
+        color={quizzCards.libreria.color}
+        onClick={onLibraryClick}
         />
         
         <!-- <div class="w-1/2 pl-4 pr-4 pt-2 pb-2 text-center border-green-500 border-2 bg-green-100">
@@ -43,5 +63,4 @@
         <p class="text-xl">Libreria</p>
         </div> -->
     </div>
-    <!-- Gif de confetti si responde correctamente, Gif de alarma si no-->
 </article>
