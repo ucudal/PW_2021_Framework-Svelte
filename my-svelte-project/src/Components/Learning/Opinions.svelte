9d7c686e08aabb5c948cb403bc0b8c791025c476
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-10-28 20:39:31 -0300

    learning - fixes

--- my-svelte-project/src/Components/Learning/Opinions.svelte
+++ my-svelte-project/src/Components/Learning/Opinions.svelte
@@ -1,23 +1,34 @@
 <script>
-    // let tutorial_name = 'Tutorial'
-    // let tutorial_text = "El tutorial me parecio bastante " +
-    // "accesible, aunque parece ser muy basico, en internet "+
-    // "se puede encontrar mas informacion y buenos ejemplos"
+    let tutorial_name = 'Tutorial'
+    let tutorial_text = "El tutorial me parecio bastante" +
+    " accesible, aunque parece ser muy basico, en internet"+
+    " se puede encontrar mas informacion y buenos ejemplos"
 
-    // let loPrimero_name = 'Lo primero que pense'
-    // let loPrimero_text = "Mi primer impresion fue que estaba muy bueno"+
-    // ", se pueden obtener resultados rapidamente y con pocas lineas de codigo."+
-    // " Por lo que pude ver de otros frameworks, Svelte utiliza una sintaxis muy"+
-    // " similar a html y javascript, por lo que hace que sea mas facil aprenderlo."
+    let loPrimero_name = 'Lo primero que pense'
+    let loPrimero_text = "Mi primer impresion fue que estaba muy bueno"+
+    ", se pueden obtener resultados rapidamente y con pocas lineas de codigo."+
+    " Por lo que pude ver de otros frameworks, Svelte utiliza una sintaxis muy"+
+    " similar a html y javascript, por lo que hace que sea mas facil aprenderlo."
 
-    // export let opinions;
-    // opinions = [
-    //     {name: tutorial_name, text: tutorial_text, showed: false},
-    //     {name: loPrimero_name, text: loPrimero_text, showed: false},
-    // ];
+    let internet_name = 'Segun internet...'
+    let internet_text = "Es uno de los mas usados para crear apps facilmente"+
+    ", optimiza las aplicaciones porque no utiliza DOM virtual."+
+    "  Se volvio bastante popular... "
 
+    let resumen_name = 'Resumiendo...'
+    let resumen_text = "Me gusto bastante, es bien sencillo, "+
+    " obvio apenas lo conozco pero valoro haber conocido esta herramienta."
 
-    // Mi idea era definir aca las opiniones estaticas, 
-    // y mostrarlas del otro lado, pero no me salio ... luego veo si me sale
-    
+    export let id;
+    let opinions = [
+        {id: 0, name: tutorial_name, text: tutorial_text},
+        {id: 1, name: loPrimero_name, text: loPrimero_text},
+        {id: 2, name: internet_name, text: internet_text},
+        {id: 3, name: resumen_name, text: resumen_text},
+    ];
+
 </script>
+<article class="p-5 m-5 bg-red-200 rounded-full py-3 px-6">
+    <h1>{opinions[id].name}</h1>
+    <h3>{opinions[id].text}</h3>
+</article>
--- my-svelte-project/src/Pages/Learning.svelte
+++ my-svelte-project/src/Pages/Learning.svelte
@@ -1,68 +1,44 @@
 <script>
-  // import Opinions from "../Components/Learning/Opinions.svelte";
+    import Opinions from "../Components/Learning/Opinions.svelte";
 
-    let tutorial_name = 'Tutorial'
-    let tutorial_text = "El tutorial me parecio bastante" +
-    " accesible, aunque parece ser muy basico, en internet"+
-    " se puede encontrar mas informacion y buenos ejemplos"
-
-    let loPrimero_name = 'Lo primero que pense'
-    let loPrimero_text = "Mi primer impresion fue que estaba muy bueno"+
-    ", se pueden obtener resultados rapidamente y con pocas lineas de codigo."+
-    " Por lo que pude ver de otros frameworks, Svelte utiliza una sintaxis muy"+
-    " similar a html y javascript, por lo que hace que sea mas facil aprenderlo."
-
-    let internet_name = 'Segun internet...'
-    let internet_text = "Es uno de los mas usados para crear apps facilmente"+
-    ", optimiza las aplicaciones porque no utiliza DOM virtual."+
-    "  Se volvio bastante popular... "
-
-    let resumen_name = 'Resumiendo...'
-    let resumen_text = "Me gusto bastante, es bien sencillo, "+
-    " obvio apenas lo conozco pero valoro haber conocido esta herramienta."
+    let contador = 0
+    const aumentar = () => {
+      if (contador < 3) { 
+        contador ++ 
+    
+      } else {
+        contador =0;
+      }
+    }
+    const decrementar = () => {
+      if (contador > 0) { 
+        contador -- 
+    
+      } else {
+        contador = 3;
+      }
+    }
 
 
-    // export let opinions;
-    let opinions = [
-        {name: tutorial_name, text: tutorial_text, showed: false},
-        {name: loPrimero_name, text: loPrimero_text, showed: false},
-        {name: internet_name, text: internet_text, showed: false},
-        {name: resumen_name, text: resumen_text, showed: false},
-    ];
-
-  const verSiguiente = op_name => {
-      op = opinions.filter(item => item.name == op_name)
-      op.showed = true
-      opinions = [rest_op, op]
-      return opinions
-
-  }
+    
 
 </script>
 
-<div >
-    <div class="grid justify-items-center">
-      <h1 class="font-sans">Curva de aprendizaje!</h1>
-    </div>
-
-
-    {#each opinions as op }
-      {#if !op.showed}
-        <div class="container mx-auto">
-          <p class="font-sans">{op.name}</p>
-        </div>
-
-        <div >
-          <p class="font-sans">
-            {op.text}
-          </p>
-        </div>
-        <button on:click="{verSiguiente(op.name)}">
-          Ver siguiente
-        </button>
-      {/if}
-
-    {/each}
-
+<article>
+  <div class="grid justify-items-center">
+    <h2 class="text-2xl mb-3">Curva de aprendizaje!</h2>
   </div>
+
+  <Opinions id={contador}/>
+  <div class="grid justify-items-center">
+    <div class="flex">
+      <div>
+        <button on:click={decrementar}>Anterior</button>
+      </div>
+      <div>
+        <button on:click={aumentar}>Siguiente</button>
+      </div>
+    </div>
+  </div>
+</article>
 
