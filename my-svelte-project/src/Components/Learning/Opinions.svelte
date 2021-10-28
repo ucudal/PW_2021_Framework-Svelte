48d62ff5f24b3b33147ddcd3847c0dd4d8b6cbac
AnaBergallo <ana.bergallo@correo.ucu.edu.uy>
2021-10-28 02:14:04 -0300

    learning : pendiente que funcione la logica

---
+++ my-svelte-project/src/Components/Learning/Opinions.svelte
@@ -0,0 +1,23 @@
+<script>
+    // let tutorial_name = 'Tutorial'
+    // let tutorial_text = "El tutorial me parecio bastante " +
+    // "accesible, aunque parece ser muy basico, en internet "+
+    // "se puede encontrar mas informacion y buenos ejemplos"
+
+    // let loPrimero_name = 'Lo primero que pense'
+    // let loPrimero_text = "Mi primer impresion fue que estaba muy bueno"+
+    // ", se pueden obtener resultados rapidamente y con pocas lineas de codigo."+
+    // " Por lo que pude ver de otros frameworks, Svelte utiliza una sintaxis muy"+
+    // " similar a html y javascript, por lo que hace que sea mas facil aprenderlo."
+
+    // export let opinions;
+    // opinions = [
+    //     {name: tutorial_name, text: tutorial_text, showed: false},
+    //     {name: loPrimero_name, text: loPrimero_text, showed: false},
+    // ];
+
+
+    // Mi idea era definir aca las opiniones estaticas, 
+    // y mostrarlas del otro lado, pero no me salio ... luego veo si me sale
+    
+</script>
--- my-svelte-project/src/Pages/Learning.svelte
+++ my-svelte-project/src/Pages/Learning.svelte
@@ -1,3 +1,68 @@
-<div>
-  <p>Curva de aprendizaje!</p>
-</div>
+<script>
+  // import Opinions from "../Components/Learning/Opinions.svelte";
+
+    let tutorial_name = 'Tutorial'
+    let tutorial_text = "El tutorial me parecio bastante" +
+    " accesible, aunque parece ser muy basico, en internet"+
+    " se puede encontrar mas informacion y buenos ejemplos"
+
+    let loPrimero_name = 'Lo primero que pense'
+    let loPrimero_text = "Mi primer impresion fue que estaba muy bueno"+
+    ", se pueden obtener resultados rapidamente y con pocas lineas de codigo."+
+    " Por lo que pude ver de otros frameworks, Svelte utiliza una sintaxis muy"+
+    " similar a html y javascript, por lo que hace que sea mas facil aprenderlo."
+
+    let internet_name = 'Segun internet...'
+    let internet_text = "Es uno de los mas usados para crear apps facilmente"+
+    ", optimiza las aplicaciones porque no utiliza DOM virtual."+
+    "  Se volvio bastante popular... "
+
+    let resumen_name = 'Resumiendo...'
+    let resumen_text = "Me gusto bastante, es bien sencillo, "+
+    " obvio apenas lo conozco pero valoro haber conocido esta herramienta."
+
+
+    // export let opinions;
+    let opinions = [
+        {name: tutorial_name, text: tutorial_text, showed: false},
+        {name: loPrimero_name, text: loPrimero_text, showed: false},
+        {name: internet_name, text: internet_text, showed: false},
+        {name: resumen_name, text: resumen_text, showed: false},
+    ];
+
+  const verSiguiente = op_name => {
+      op = opinions.filter(item => item.name == op_name)
+      op.showed = true
+      opinions = [rest_op, op]
+      return opinions
+
+  }
+
+</script>
+
+<div >
+    <div class="grid justify-items-center">
+      <h1 class="font-sans">Curva de aprendizaje!</h1>
+    </div>
+
+
+    {#each opinions as op }
+      {#if !op.showed}
+        <div class="container mx-auto">
+          <p class="font-sans">{op.name}</p>
+        </div>
+
+        <div >
+          <p class="font-sans">
+            {op.text}
+          </p>
+        </div>
+        <button on:click="{verSiguiente(op.name)}">
+          Ver siguiente
+        </button>
+      {/if}
+
+    {/each}
+
+  </div>
+
